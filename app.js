21ea20a1f11ddae4d0e7e0e1acb62615a947aa01
kasuri <hbkasuri@gmail.com>
2021-01-26 21:59:24 +0530

    kasuriMyPortfolioDesignUsingScretch-v1

---
+++ app.js
@@ -0,0 +1,40 @@
+var show = document.getElementById("nav-links");
+
+function showMenu() {
+  show.style.right = "0";
+}
+
+function closeMenu() {
+  show.style.right = "-200px";
+}
+
+function aboutMeTabController(tabName) {
+  var i;
+  var x = document.getElementsByClassName("tabn");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
